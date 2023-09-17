86896c53e53f28c91d7855569aca2e2bc65662d8
MiliRamirezLarre <131774533+MiliRamirezLarre@users.noreply.github.com>
2023-09-16 23:19:38 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// script.js
+
+// Obtener una referencia al botón
+const botonInvitado = document.getElementById("ingresarComoInvitado");
+
+// Agregar un manejador de eventos de clic al botón
+botonInvitado.addEventListener("click", function() {
+    // Coloca aquí el código que deseas ejecutar cuando se hace clic en el botón.
+    // Puede ser una redirección, mostrar un mensaje, etc.
+    alert("Has ingresado como invitad@");
+    // Otra acción deseada...
+});
---
+++ script2.js
@@ -0,0 +1,22 @@
+// const GENERAR = document.getElementById('generar');
+// const DATO = document.getElementById('dato');
+
+// const VER_REM_BTN = document.getElementById('ver-rem');
+
+// GENERAR.addEventListener('click', () => {
+    
+
+   
+// };
+
+function verMiNombre(){
+    let dato = document.getElementById('dato').value;
+    let nombre_remera = document.getElementById('nombre-remera');
+    nombre_remera.innerHTML = dato;
+}
+function verMiNum(){
+    let num = document.getElementById('num').value;
+    let numerodos = document.getElementById('numerodos');
+    numerodos.innerHTML = num;
+}
+
